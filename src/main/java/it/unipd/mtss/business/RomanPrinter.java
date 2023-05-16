ab133d5efefcab5047d1c08356cb63507382ff8b
Rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2023-05-16 15:44:49 +0200

    Stampa primi 10 numeri romani
    Close #8

--- src/main/java/it/unipd/mtss/business/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/business/RomanPrinter.java
@@ -10,6 +10,39 @@ import java.util.Objects;
 
 public class RomanPrinter {
 
+  static int positionArray[];
+  static String romanSymbol[][];
+
+  public RomanPrinter()
+  {
+    positionArray = new int[100];
+    romanSymbol = new String[6][3];
+
+    positionArray[73] = 0;
+    romanSymbol[0][0] = " _____ "; 
+    romanSymbol[1][0] = "|_   _|";  
+    romanSymbol[2][0] = "  | |  "; 
+    romanSymbol[3][0] = "  | |  ";
+    romanSymbol[4][0] = " _| |_ "; 
+    romanSymbol[5][0] = "|_____|"; 
+
+    positionArray[86] = 1;
+    romanSymbol[0][1] = "__      __"; 
+    romanSymbol[1][1] = "\\ \\    / /";
+    romanSymbol[2][1] = " \\ \\  / / "; 
+    romanSymbol[3][1] = "  \\ \\/ /  "; 
+    romanSymbol[4][1] = "   \\  /   "; 
+    romanSymbol[5][1] = "    \\/    ";
+
+    positionArray[88] = 2;
+    romanSymbol[0][2] = "__   __"; 
+    romanSymbol[1][2] = "\\ \\ / /";
+    romanSymbol[2][2] = " \\ V / "; 
+    romanSymbol[3][2] = "  > <  "; 
+    romanSymbol[4][2] = " / . \\ "; 
+    romanSymbol[5][2] = "/_/ \\_\\";
+  }
+
   public static String print(int num) 
   throws RomanNumberException {
     return printAsciiArt(IntegerToRoman.convert(num));
@@ -43,6 +76,25 @@ public class RomanPrinter {
         "The string contains incompatible characters"
       );
     }
-    return null;
+    
+    String romanAsciiArt = "";
+
+    for (int i = 0; i < 6; i++)
+    {
+      for (int j = 0; j < romanNumber.length(); j++)
+      {
+        romanAsciiArt += romanSymbol[i][positionArray[(romanNumber.charAt(j))]];
+
+        if (j + 1 != romanNumber.length())
+        {
+          romanAsciiArt += " ";
+        }
+      }
+      if (i + 1 != 6)
+      {
+        romanAsciiArt += "\n";
+      }
+    }
+    return romanAsciiArt;
   }
 }
